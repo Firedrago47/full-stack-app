7dab887a3bd46445a76f604475fb2eab4982436b
Deep <deepanrajpmk@gmail.com>
2026-01-06 21:35:12 +0530

    style:timeline for pick drop

--- src/app/customer/components/TaxiSection.tsx
+++ src/app/customer/components/TaxiSection.tsx
@@ -49,22 +49,37 @@ export default function TaxiSection() {
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {/* LEFT: Location inputs */}
-        <div className="bg-white border rounded-xl p-4 shadow-sm space-y-4">
-          <h3 className="text-lg font-semibold">Trip Details</h3>
+        <div className="bg-white border rounded-xl p-4 shadow-sm">
+          <h3 className="text-lg font-semibold mb-6">Trip Details</h3>
 
-          <div className="border-l pl-4 pr-4 space-y-6 border-dashed border-black ml-1" >
+          <div className="flex ml-2 gap-4">
+            {/* Timeline */}
+            <div className="flex flex-col items-center pt-4 pb-6 ">
+              {/* Pickup dot */}
+              <div className="h-3 w-3 rounded-full bg-green-600" />
 
-          <LocationAutocomplete 
-            label="Pickup location"
-            value={pickup?.address ?? ""}
-            onSelect={setPickup}
-            />
-          <LocationAutocomplete
-            label="Drop location"
-            value={drop?.address ?? ""}
-            onSelect={setDrop}
-            />
+              {/* Line */}
+              <div className="flex-1 w-px border-l-2 border-dashed border-gray-400 my-2" />
+
+              {/* Drop dot */}
+              <div className="h-3 w-3 rounded-full bg-red-600" />
             </div>
+
+            {/* Inputs */}
+            <div className="flex flex-col gap-4 mr-4 flex-1">
+              <LocationAutocomplete
+                label="Pickup location"
+                value={pickup?.address ?? ""}
+                onSelect={setPickup}
+              />
+
+              <LocationAutocomplete
+                label="Drop location"
+                value={drop?.address ?? ""}
+                onSelect={setDrop}
+              />
+            </div>
+          </div>
         </div>
 
         {/* RIGHT: Map */}
