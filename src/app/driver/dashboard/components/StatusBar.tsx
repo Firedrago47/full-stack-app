c7d51e95c14739f58648ea74e98edfdb027c8d1b
Deep <deepanrajpmk@gmail.com>
2026-02-05 01:09:45 +0530

    feat:statusbar impr in driver

--- src/app/driver/dashboard/components/StatusBar.tsx
+++ src/app/driver/dashboard/components/StatusBar.tsx
@@ -1,3 +1,5 @@
+import { ArrowDownNarrowWide, ArrowUpNarrowWide } from "lucide-react";
+
 interface Props {
   todayEarningsCents: number;
 }
@@ -6,6 +8,7 @@ export default function StatusBar({
   todayEarningsCents,
 }: Props) {
   return (
+    <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
     <div className="bg-white border rounded-xl p-4 flex justify-between items-center">
       <div>
         <p className="text-sm text-muted-foreground">
@@ -19,6 +22,35 @@ export default function StatusBar({
       <span className="text-sm text-green-600 font-medium">
         Online
       </span>
+    </div>
+    <div className=" bg-white border rounded-xl p-4 flex justify-between items-center">
+      <div>
+        <p className="text-sm text-muted-foreground">
+          Total Rides Today
+        </p>
+        <p className="text-xl font-semibold">
+          7
+        </p>
+      </div>
+
+      <span className="text-sm text-red-600 font-medium">
+          <ArrowDownNarrowWide size={16} className="inline" /> 0.1%
+      </span>
+      </div>
+      <div className=" bg-white border rounded-xl p-4 flex justify-between items-center">
+      <div>
+        <p className="text-sm text-muted-foreground">
+          Rating
+        </p>
+        <p className="text-xl font-semibold">
+          4.9
+        </p>
+      </div>
+
+      <span className="text-sm text-green-600 font-medium">
+        <ArrowUpNarrowWide size={16} className="inline" /> 0.1%
+      </span>
+      </div>
     </div>
   );
 }
